0707eac6d18df06e7aba67ea112ac73a93d9065d
jnack <55568980+jnackmclain@users.noreply.github.com>
2023-02-24 12:25:27 -0600

    mimic rb3dx dependencies layout

--- _build_xbox.bat
+++ _build_xbox.bat
@@ -1,7 +1,7 @@
 set FAILED_ARK_BUILD=0
 @echo off
 echo:Building Xbox ARK
-"%~dp0dependencies/arkhelper" dir2ark "%~dp0_ark" "%~dp0_build\xbox\gen" -n "dxsl_xbox" -e -v 5 -s 4073741823 >nul
+"%~dp0dependencies/windows/arkhelper" dir2ark "%~dp0_ark" "%~dp0_build\xbox\gen" -n "dxsl_xbox" -e -v 5 -s 4073741823 >nul
 if %errorlevel% neq 0 (set FAILED_ARK_BUILD=1)
 if %FAILED_ARK_BUILD% neq 1 (echo:Successfully built Loader ARK. You may find the files needed to place on your Xbox 360 in /_build/Xbox/)
 if %FAILED_ARK_BUILD% neq 0 (echo:Error building ARK. Check your modifications or run _git_reset.bat to rebase your repo.)
--- _build_xenia.bat
+++ _build_xenia.bat
@@ -2,7 +2,7 @@ set FAILED_ARK_BUILD=0
 @echo off
 echo:
 echo:Building Xbox ARK
-"%~dp0dependencies/arkhelper" dir2ark "%~dp0_ark" "%~dp0_build\xbox\gen" -n "dxsl_xbox" -e -v 5 -s 4073741823 >nul
+"%~dp0dependencies/windows/arkhelper" dir2ark "%~dp0_ark" "%~dp0_build\xbox\gen" -n "dxsl_xbox" -e -v 5 -s 4073741823 >nul
 if %errorlevel% neq 0 (set FAILED_ARK_BUILD=1)
 echo:
 if %FAILED_ARK_BUILD% neq 1 (echo:Successfully built Loader ARK.)
