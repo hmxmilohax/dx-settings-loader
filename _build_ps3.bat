2e87b70375f434d7a9269a70df04ae5cd86f9df4
jnack <55568980+jnackmclain@users.noreply.github.com>
2023-04-14 19:50:05 -0500

    update layout

--- _build_ps3.bat
+++ _build_ps3.bat
@@ -5,7 +5,7 @@ echo:Temporarily moving Xbox files out of the ark path to reduce final ARK size
 @%SystemRoot%\System32\robocopy.exe "%~dp0_ark" "%~dp0_temp\_ark" *.png_xbox /S /MOVE /XD "%~dp0_temp\_ark" /NDL /NFL /NJH /NJS /R:0 >nul
 @%SystemRoot%\System32\robocopy.exe "%~dp0_ark" "%~dp0_temp\_ark" *.bmp_xbox /S /MOVE /XD "%~dp0_temp\_ark" /NDL /NFL /NJH /NJS /R:0 >nul
 echo:Building DX Settings Loader arks.
-"%~dp0dependencies/windows/arkhelper" dir2ark "%~dp0_ark" "%~dp0_build\ps3\USRDIR\gen" -n "main_ps3" -e -v 5 -s 4073741823 >nul
+"%~dp0dependencies/windows/arkhelper" dir2ark "%~dp0_ark" "%~dp0_build\ps3\PS3_GAME\USRDIR\gen" -n "main_ps3" -e -v 5 -s 4073741823 >nul
 if %errorlevel% neq 0 (set FAILED_ARK_BUILD=1)
 if %FAILED_ARK_BUILD% neq 1 (echo:Successfully built Loader ARK. You may find the files needed to place on your PS3 in /_build/PS3/)
 if %FAILED_ARK_BUILD% neq 0 (echo:Error building ARK. Check your modifications or run _git_reset.bat to rebase your repo.)
