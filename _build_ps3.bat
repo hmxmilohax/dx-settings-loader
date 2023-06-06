74bbe0251f47e118c5596aab76b9e5f91318c7a0
Luna <luna.abarta@gmail.com>
2023-06-05 20:43:11 -0700

    better build scripts

--- _build_ps3.bat
+++ _build_ps3.bat
@@ -1,18 +1,21 @@
 set FAILED_ARK_BUILD=0
+git pull https://github.com/hmxmilohax/dx-settings-loader main
 @echo off
+echo:
 echo:Temporarily moving Xbox files out of the ark path to reduce final ARK size
-@%SystemRoot%\System32\robocopy.exe "%~dp0_ark" "%~dp0_temp\_ark" *.milo_xbox /S /MOVE /XD "%~dp0_temp\_ark" /NDL /NFL /NJH /NJS /R:0 >nul
-@%SystemRoot%\System32\robocopy.exe "%~dp0_ark" "%~dp0_temp\_ark" *.png_xbox /S /MOVE /XD "%~dp0_temp\_ark" /NDL /NFL /NJH /NJS /R:0 >nul
-@%SystemRoot%\System32\robocopy.exe "%~dp0_ark" "%~dp0_temp\_ark" *.bmp_xbox /S /MOVE /XD "%~dp0_temp\_ark" /NDL /NFL /NJH /NJS /R:0 >nul
-echo:Building DX Settings Loader arks.
+@%SystemRoot%\System32\robocopy.exe "%~dp0\_ark" "%~dp0_temp\_ark" *.milo_xbox /S /MOVE /XD "%~dp0_temp\_ark" /NDL /NFL /NJH /NJS /R:0 >nul
+@%SystemRoot%\System32\robocopy.exe "%~dp0\_ark" "%~dp0_temp\_ark" *.png_xbox /S /MOVE /XD "%~dp0_temp\_ark" /NDL /NFL /NJH /NJS /R:0 >nul
+echo:
+echo:Building PS3 ARK
 "%~dp0dependencies/windows/arkhelper" dir2ark "%~dp0_ark" "%~dp0_build\ps3\PS3_GAME\USRDIR\gen" -n "main_ps3" -e -v 5 -s 4073741823 >nul
 if %errorlevel% neq 0 (set FAILED_ARK_BUILD=1)
-if %FAILED_ARK_BUILD% neq 1 (echo:Successfully built Loader ARK. You may find the files needed to place on your PS3 in /_build/PS3/)
-if %FAILED_ARK_BUILD% neq 0 (echo:Error building ARK. Check your modifications or run _git_reset.bat to rebase your repo.)
+echo:
 echo:Moving back Xbox files
-@%SystemRoot%\System32\robocopy.exe "%~dp0_temp\_ark" "%~dp0_ark" *.milo_xbox /S /MOVE /XD "%~dp0_ark" /NDL /NFL /NJH /NJS /R:0 >nul
-@%SystemRoot%\System32\robocopy.exe "%~dp0_temp\_ark" "%~dp0_ark" *.png_xbox /S /MOVE /XD "%~dp0_ark" /NDL /NFL /NJH /NJS /R:0 >nul
-@%SystemRoot%\System32\robocopy.exe "%~dp0_temp\_ark" "%~dp0_ark" *.bmp_xbox /S /MOVE /XD "%~dp0_ark" /NDL /NFL /NJH /NJS /R:0 >nul
+@%SystemRoot%\System32\robocopy.exe "%~dp0_temp\_ark" "%~dp0\_ark" *.milo_xbox /S /MOVE /XD "%~dp0_ark" /NDL /NFL /NJH /NJS /R:0 >nul
+@%SystemRoot%\System32\robocopy.exe "%~dp0_temp\_ark" "%~dp0\_ark" *.png_xbox /S /MOVE /XD "%~dp0_ark" /NDL /NFL /NJH /NJS /R:0 >nul
 rd _temp
+echo:
+if %FAILED_ARK_BUILD% neq 1 (echo:Successfully built DX Settings Loader ARK. You may find the files needed to place on your PS3 in /_build/ps3/)
+if %FAILED_ARK_BUILD% neq 0 (echo:Error building ARK. Check your modifications or run _git_reset.bat to rebase your repo.)
 echo:
 pause
--- _build_xenia.bat
+++ _build_xenia.bat
@@ -1,10 +1,23 @@
 set FAILED_ARK_BUILD=0
+git pull https://github.com/hmxmilohax/dx-settings-loader main
 @echo off
+echo:
+echo:Temporarily moving PS3 files out of the ark path to reduce final ARK size
+@%SystemRoot%\System32\robocopy.exe "%~dp0\_ark" "%~dp0_temp\_ark" *.milo_ps3 /S /MOVE /XD "%~dp0_temp\_ark" /NDL /NFL /NJH /NJS /R:0 >nul
+@%SystemRoot%\System32\robocopy.exe "%~dp0\_ark" "%~dp0_temp\_ark" *.png_ps3 /S /MOVE /XD "%~dp0_temp\_ark" /NDL /NFL /NJH /NJS /R:0 >nul
+echo:
 echo:Building Xbox ARK
 "%~dp0dependencies/windows/arkhelper" dir2ark "%~dp0_ark" "%~dp0_build\xbox\gen" -n "dxsl_xbox" -e -v 5 -s 4073741823 >nul
 if %errorlevel% neq 0 (set FAILED_ARK_BUILD=1)
-if %FAILED_ARK_BUILD% neq 1 (echo:Successfully built Loader ARK. You may find the files needed to place on your Xbox 360 in /_build/Xbox/)
-if %FAILED_ARK_BUILD% neq 0 (echo:Error building ARK. Check your modifications or run _git_reset.bat to rebase your repo.)
-"%~dp0\_xenia\xenia_canary" "%~dp0\_build\xbox\dx-settings-loader.xex"
 echo:
-pause
+echo:Moving back PS3 files
+@%SystemRoot%\System32\robocopy.exe "%~dp0_temp\_ark" "%~dp0\_ark" *.milo_ps3 /S /MOVE /XD "%~dp0_ark" /NDL /NFL /NJH /NJS /R:0 >nul
+@%SystemRoot%\System32\robocopy.exe "%~dp0_temp\_ark" "%~dp0\_ark" *.png_ps3 /S /MOVE /XD "%~dp0_ark" /NDL /NFL /NJH /NJS /R:0 >nul
+rd _temp
+echo:
+if %FAILED_ARK_BUILD% neq 1 (echo:Successfully built DX Settings Loader ARK.)
+if %FAILED_ARK_BUILD% neq 1 (echo:)
+if %FAILED_ARK_BUILD% neq 0 (echo:Error building ARK. Check your modifications or run _git_reset.bat to rebase your repo.)
+if %FAILED_ARK_BUILD% neq 1 (echo:Launching Xenia & START "" "%~dp0\_xenia\xenia_canary" "%~dp0\_build\xbox\dx-settings-loader.xex")
+echo:
+if %FAILED_ARK_BUILD% neq 0 (pause)
